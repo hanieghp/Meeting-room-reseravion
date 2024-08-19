a7553ffaf5da781b15daf01fd68d9a792daac19b
hanieghp <hanie.ghp82@gmail.com>
2024-08-19 12:35:17 +0430

    manager interface methode updated

--- src/main/java/org/example/Impl/ManagerImpl.java
+++ src/main/java/org/example/Impl/ManagerImpl.java
@@ -1,11 +1,29 @@
 package org.example.Impl;
 
+import org.example.entity.Room;
 import org.example.entity.User;
 import org.example.interfaces.ManagerInterface;
+
+import java.util.List;
 
 public class ManagerImpl extends User implements ManagerInterface {
 
     public ManagerImpl(int userId, String password, String firstName, String lastName, String roll) {
         super(userId, password, firstName, lastName, roll);
+    }
+
+    @Override
+    public List<Room> getAllRooms() {
+        return List.of();
+    }
+
+    @Override
+    public boolean addRoom(Room room) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteRoom(int roomId) {
+        return false;
     }
 }
--- src/main/java/org/example/interfaces/ManagerInterface.java
+++ src/main/java/org/example/interfaces/ManagerInterface.java
@@ -1,11 +1,16 @@
 package org.example.interfaces;
 
+import org.example.entity.Room;
+import java.util.List;
+
 public interface ManagerInterface {
 
-    // Define a List of Room to show
+    // Method to retrieve list of all rooms (both occupied and empty)
+    List<Room> getAllRooms();
 
+    // Method to add a new room to the database
+    boolean addRoom(Room room);
 
-    // Add to list of Room
-
-    // Delete from Room list
+    // Method to delete a room from the database
+    boolean deleteRoom(int roomId);
 }
